06e689146a4351868ab7f352dc1da3ecbcf602b5
webdevprofesh <webdevprofesh@gmail.com>
2016-01-31 15:18:56 -0800

    Update page.php

--- WP Starter Theme/page.php
+++ WP Starter Theme/page.php
@@ -3,30 +3,17 @@
 * Template Name: Default Page Template
 */
 ?>
+<?php get_header(); ?>
+<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
 
-<?php get_header(''); ?>
+<main>
 
-<div class="container">
-        <div class="row">
+</main>
 
-		<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
+<?php endwhile; else: ?>
+    <p><?php _e('No posts were found. Sorry!'); ?></p>
+<?php endif; ?>
+<?php wp_footer(); ?>
 
-		<div class="col-md-12">
-                <h1><?php the_title(); ?></h1>
-
-				<?php the_content('Read More...'); ?>
-		</div>
-
-		<?php endwhile; else: ?>
-			<p><?php _e('No posts were found. Sorry!'); ?></p>
-		<?php endif; ?>
-
-	</div>
-
-</div>
-
-
-
-
-
-<?php get_footer(''); ?>
+</body>
+</html>
