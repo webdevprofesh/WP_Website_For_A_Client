e6912835d84a5e74d5015a7e2b5b256c5fac74c3
webdevprofesh <webdevprofesh@gmail.com>
2016-01-31 15:12:09 -0800

    Update 404.php

--- WP Starter Theme/404.php
+++ WP Starter Theme/404.php
@@ -21,7 +21,7 @@
                 <p class="open-sans">If not, just head on back to the <a href="<?php echo home_url() ?>" style="color:blue">Home Page</a>.</p>
                 <h6 class="open-sans">One more thing:</h6>
                 <p class="open-sans">It would be awesome if you could help us fix this issue with a bit of feedback. Please, contact us and let us know what went wrong. Oh, and it's best if you can tell us what Web Browser and Operating System you were using when the error occurred. Thank you!</p>
-                <p class="open-sans">Email: Kyle@webdevprofesh.com</p>
+                <p class="open-sans">Email: <?php $admin_email = get_option( 'admin_email' ); ?></p>
             </div>
         </div>
     </div>
